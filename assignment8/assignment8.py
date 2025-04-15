16b6b23f10f7c77ef12e05384ba33921bafda028
crystal <itscrystalm@gmail.com>
2025-04-15 15:58:31 -0500

    15-april-2025-1558

--- assignment8/assignment8.py
+++ assignment8/assignment8.py
@@ -1,3 +1,13 @@
+# ------------------------------
+# GLOBAL VARIABLES
+# ------------------------------
+
+flat_size = 40
+
+mkv_size = 7
+
+chain_len = 20
+
 
 # ----------------------------------
 # PYTHON CODE
@@ -6,15 +16,11 @@
 import func_lib
 import random
 
-powers = [ 0 , 1 , 2 , 3 , 4  ]
-
-dur_bank = [ 2**x for x in powers]
-
-dur_bankf = [ 1/(2**x) for x in powers ]
+dur_bank = [ 2**x for x in range(5) ]
 
 pit_bank = [ 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'c\'', 'd\'', 'e\'', 'f\'', 'g\'', 'a\'', 'b\'', 'c\'' ]
 
-dyn_bank = [ -2 , -1 , 0 , 1 , 2 , 3 ]
+dyn_bank = ['pp' , 'p' , 'mp'  , 'mf' , 'f' , 'ff' ]
 
 
 
@@ -22,49 +28,30 @@ dyn_bank = [ -2 , -1 , 0 , 1 , 2 , 3 ]
 # - FLAT RANDOM CHOICE SECTION
 # -----------------------------------
 
-dur_40 = [ random.choice(dur_bank) for i in range(40)  ]
+dur_len = [ random.choice(dur_bank) for i in range(flat_size)  ]
 
-# print (dur_40)
+pit_len = [ random.choice(pit_bank) for i in range(flat_size) ]
 
-pit_40 = [ random.choice(pit_bank) for i in range(40) ]
+dyn_toggle = random.sample( range(flat_size), 8 )
 
-# print (pit_40)
+dyn_len = [ random.choice(dyn_bank) if i in dyn_toggle else '' for i in range(flat_size) ]
 
-random_40 = [ pit_40[i] + str(dur_40[i]) for i in range(len(pit_40)) ]
-
-# print (lsprint)
-
-
-# print ('done')
-
-# lsprintfile.close()
-
+random_len = [ pit_len[i] + str(dur_len[i]) + dyn_len[i] for i in range(flat_size) ]
 
 # -------------------------------
 # - MARKOV SECTION
 # -------------------------------
 
-pit_dur_together = [ pit_40[i] + str(dur_40[i]) for i in range(len(dur_40)) ]
-
-mkv_bank = pit_dur_together[0:7]
-
-print (mkv_bank)
+mkv_bank = random_len[0:mkv_size]
 
 mkv_matrix = func_lib.mkmatrix(len(mkv_bank))
 
-print (mkv_matrix)
-
 # - chain
 
-mkv_chain = func_lib.mkvchn(20, mkv_matrix)
-
-print (mkv_chain)
+mkv_chain = func_lib.mkvchn(chain_len, mkv_matrix)
 
 mkv_chain_render = [ mkv_bank[mkv_chain[item]] for item in mkv_chain ]
 
-# print (chain_bank_render)
-
-print (func_lib.lyprint(mkv_chain_render))
 
 # ---------------------------------------
 # - WRITING LILYPOND FILE
@@ -95,7 +82,8 @@ ass8_flat_random_printfile = open('ass8-flat-random.ly', 'w')
 
 ass8_flat_random_printfile.write(preamble)
 
-ass8_flat_random_printfile.writelines(func_lib.lyprint(random_40))
+ass8_flat_random_printfile.writelines(func_lib.lyprint(random_len))
+
 
 
 # - CLOSES STAFF AND SCORE ENVIRONMENTS
@@ -110,7 +98,6 @@ ass8_flat_random_printfile.close()
 
 
 
-
 # - OPEN ASS8-MARKOV-BANK.LY AND WRITE PREAMBLE
 
 ass8_markov_bank_printfile = open('ass8-markov-bank.ly', 'w')
@@ -119,7 +106,7 @@ ass8_markov_bank_printfile.write(preamble)
 
 ass8_markov_bank_printfile.writelines(func_lib.lyprint(mkv_bank))
 
-print (func_lib.lyprint(mkv_bank))
+# print (func_lib.lyprint(mkv_bank))
 
 ass8_markov_bank_printfile.write('}\n')
 
@@ -142,11 +129,11 @@ ass8_markov_chain_printfile = open('ass8-markov-chain.ly', 'w')
 
 ass8_markov_chain_printfile.write(preamble)
 
-lyprint_mkv_bank = [ mkv_bank[i] + '-\"' + str(i) + '\"' for i in range(len(mkv_bank)) ]
+lyprint_mkv_bank = [ mkv_bank[i] + '-\"' + str(i) + '\"' for i in range(mkv_size) ]
 
 ass8_markov_chain_printfile.writelines(func_lib.lyprint(lyprint_mkv_bank))
 
-print (func_lib.lyprint(lyprint_mkv_bank))
+# print (func_lib.lyprint(lyprint_mkv_bank))
 
 ass8_markov_chain_printfile.write('}\n')
 
@@ -163,10 +150,21 @@ ass8_markov_bank_printfile.close()
 
 
 
-# -------------------------------
-# - MARKOV CHAIN TABLE CSV
-# -------------------------------
+# ---------------------------------------
+# - WRITING MARKOV CHAIN TABLE CSV
+# ---------------------------------------
 
+ass8_markov_table_printfile = open('ass8-markov-table.csv','w')
 
-# ass8_markov_table_printfile = open('ass8-markov-table.csv','w')
+# print (mkv_matrix)
 
+matrix_string = '1, 2, 3, 4, 5, 6, 7\n'
+
+for line in range(mkv_size):
+	for element in range(mkv_size):
+		matrix_string = matrix_string + str( mkv_matrix[line][element] ) + ', '
+	matrix_string = matrix_string + '\n'
+
+ass8_markov_table_printfile.write(matrix_string)
+
+ass8_markov_table_printfile.close()
